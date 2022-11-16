1c3b89accedaba04923d7363e2efc139e3ffaf87
humbertovnavaro <humberto.navarro.dev@gmail.com>
2022-11-15 22:59:00 -0800

    add axe on start

--- overrides/kubejs/server_scripts/player-logged-in.js
+++ overrides/kubejs/server_scripts/player-logged-in.js
@@ -6,6 +6,7 @@ onEvent('player.logged_in', event => {
         event.player.give('1x minecraft:wooden_pickaxe');
         event.player.give('1x minecraft:wooden_sword');
         event.player.give('1x minecraft:wooden_shovel');
+        event.player.give('1x minecraft:wooden_axe');
         event.player.give('create:goggles');
         event.player.give('1x croptopia:tres_leches_cake');
         event.player.give('1x farmersrespite:coffee');
